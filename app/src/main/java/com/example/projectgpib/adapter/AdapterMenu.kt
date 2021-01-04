0e7b46689484116ab57603821081f9c94feee6bf
maleficvertz96 <spicy.tonkatsu@gmail.com>
2021-01-04 13:00:28 +0700

    4 Januari 2020

--- app/src/main/java/com/example/projectgpib/adapter/AdapterMenu.kt
+++ app/src/main/java/com/example/projectgpib/adapter/AdapterMenu.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectgpib.R
 import com.example.projectgpib.model.Menu
@@ -14,6 +15,7 @@ class AdapterMenu(var data:ArrayList<Menu>):RecyclerView.Adapter<AdapterMenu.Hol
     class Holder(view: View):RecyclerView.ViewHolder(view){
         val tvNama = view.findViewById<TextView>(R.id.tv_nama)
         val imgMenu = view.findViewById<ImageView>(R.id.img_menu)
+        val layout = view.findViewById<CardView>(R.id.layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -28,6 +30,10 @@ class AdapterMenu(var data:ArrayList<Menu>):RecyclerView.Adapter<AdapterMenu.Hol
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.tvNama.text = data[position].nama
         holder.imgMenu.setImageResource(data[position].gambar)
+
+        holder.layout.setOnClickListener {
+
+        }
     }
 
 }
--- app/src/main/java/com/example/projectgpib/app/ApiService.kt
+++ app/src/main/java/com/example/projectgpib/app/ApiService.kt
@@ -6,6 +6,7 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
@@ -25,4 +26,36 @@ interface ApiService {
         @Field("email") email :String,
         @Field("password") password :String
     ):Call<ResponModel>
+
+    //menampilkan jadwal ibadah minggu
+    @GET("ibadahminggu")
+    fun getIbadahminggu():Call<ResponModel>
+
+    //menampilkan jadwal ibadah pelkat
+    @GET("ibadahpelkat")
+    fun getIbadahpelkat():Call<ResponModel>
+
+    //menampilkan jadwal kebaktian
+    @GET("kebaktian")
+    fun getKebaktian():Call<ResponModel>
+
+    //menampilkan agenda kegiatan
+    @GET("kegiatan")
+    fun getKegiatan():Call<ResponModel>
+
+    //menampilkan hut jemaat kelahiran / perkawinan
+    @GET("hut")
+    fun getHut():Call<ResponModel>
+
+    //menampilkan jadwal pembinaan pelayan
+    @GET("pembinaan")
+    fun getPembinaan():Call<ResponModel>
+
+    //menampilkan penerimaan
+    @GET("penerimaan")
+    fun getPenerimaan():Call<ResponModel>
+
+    //menampilkan jadwal ibadah minggu
+    @GET("pengeluaran")
+    fun getPengeluaran():Call<ResponModel>
 }
